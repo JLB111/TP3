28a13c745369e685cef066ca6a1cd565def33755
Jerome LB <jeromelb@Jerome-LB.local>
2016-11-17 12:09:16 -0500

    Initial commit

---
+++ while test.py
@@ -0,0 +1,45 @@
+"""
+a = []
+b = "blabla"
+
+while b != "":
+    print('veuillez entrer une valeur:')
+    b = input()
+    if b != "":
+        a.append(float(b))
+print(a)
+_______________________
+
+a=int(input('enter a number:', ))
+if a % 2:
+    print('a is a odd number')
+else:
+    print(' a is a whole number')
+
+_______________________
+
+a = 0
+print("this will tell you witch number is the largest")
+a = int(input("enter 3 numbers:", )
+print(a)
+
+_______________________
+"""
+def table(base):
+    n = 1
+    while n < 10:
+        print(n * base, end =', ')
+        n = n + 1
+
+table(3)
+
+
+def table(base, min)
+
+
+
+
+
+
+
+
